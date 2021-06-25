c9f5319b693bd2f2ec39b3a96629677e7806add6
Haitham Afifi <hafifi@mail.uni-paderborn.de>
2021-06-25 12:26:53 +0200

    add docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -37,11 +37,11 @@ release = '1.0'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-	'sphinxcontrib.mermaid',
+	#'sphinxcontrib.mermaid',
 	'sphinx.ext.autodoc',
-	#'myst_parser',
+	'myst_parser',
 	'recommonmark',
-	'sphinxcontrib.mermaid'
+	#'sphinxcontrib.mermaid'
 ]
 source_suffix = {
     '.rst': 'restructuredtext',
