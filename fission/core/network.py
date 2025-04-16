09e2e7ff6ae0d64135ddf03bef7efcca6d996333
Basil Liekens <basil@irikali.be>
2025-04-16 10:57:43 +0200

    removed some typos in __repr__
     is a property that immediately returns a boolean, hence calling it as  will attempt to call a boolean which is impossible. Furthermore,  is not a property and hence should be called. Lastly, the last for loop that was concerned with the jobs had a small error in the list comprehension.

--- fission/core/network.py
+++ fission/core/network.py
@@ -759,8 +759,8 @@ class Network():
     def __repr__(self):
         debug = "Nodes\n"
         for i in self.nodes.values():
-            debug+=f"node {i} : {i.active()} Group: {[j.name for j in i.GROUPS]}\n"
+            debug+=f"node {i} : {i.active} Group: {[j.name for j in i.GROUPS]}\n"
         debug +="\n Jobs\n"
-        for i in self.jobs.values:
-            debug+=f"job {i} : Group: {[g.name for g in g.GROUPS]}"
+        for i in self.jobs.values():
+            debug+=f"job {i} : Group: {[g.name for g in i.GROUPS]}"
         return "Network: {} Nodes | {} Jobs".format(len(self.nodes), len(self.jobs))
